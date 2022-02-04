f2d000850e588755c283be62a2a42c64ccb17c8c
AliEY0 <ali.elyousfi@outlook.be>
2022-02-04 21:45:48 +0100

    Probleem 7

---
+++ ProjectEuler/PE7.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+bool isPrime(int n) {
+  if (n <= 1)
+    return false;
+  for (int i = 2; i * i <= n; i++)
+    if (n % i == 0)
+      return false;
+
+  return true;
+}
+
+int main() {
+  int n = 0, i = 1;
+
+  while (n != 10001) {
+    i++;
+    if (isPrime(i))
+      n++;
+  }
+  cout << i << "\n";
+
+  return 0;
+}
