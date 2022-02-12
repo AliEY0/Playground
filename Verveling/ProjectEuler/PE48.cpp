adba5348863ed94127548a951492fde16817b06e
AliEY0 <ali.elyousfi@outlook.be>
2022-02-12 22:10:10 +0100

    Gebruik g++ om deze files te compilen

---
+++ Verveling/ProjectEuler/PE48.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+#define ll __int128
+
+ll MOD = 10000000000;
+std::ostream &operator<<(std::ostream &dest, __int128_t value) {
+  std::ostream::sentry s(dest);
+  if (s) {
+    __uint128_t tmp = value < 0 ? -value : value;
+    char buffer[128];
+    char *d = std::end(buffer);
+    do {
+      --d;
+      *d = "0123456789"[tmp % 10];
+      tmp /= 10;
+    } while (tmp != 0);
+    if (value < 0) {
+      --d;
+      *d = '-';
+    }
+    int len = std::end(buffer) - d;
+    if (dest.rdbuf()->sputn(d, len) != len) {
+      dest.setstate(std::ios_base::badbit);
+    }
+  }
+  return dest;
+}
+
+ll binpow(ll a, ll b) {
+  ll ans = 1;
+  while (b > 0) {
+    if (b & 1)
+      (ans *= a) %= MOD;
+
+    (a *= a) %= MOD;
+    b /= 2;
+  }
+  return ans % MOD;
+}
+
+int main() {
+  ll som = 0;
+  for (ll i = 1; i <= 1000; i++) {
+    (som += binpow(i, i)) %= MOD;
+  }
+  cout << som << "\n";
+  return 0;
+}
