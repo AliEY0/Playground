37feb0035a431ee86b2ad0dc87b8d25b79436214
AliEY0 <ali.elyousfi@outlook.be>
2022-02-13 11:38:41 +0100

    Probleem 20

--- Verveling/ProjectEuler/PE20.cpp
+++ Verveling/ProjectEuler/PE20.cpp
@@ -1,40 +1,51 @@
-
-// https://iq.opengenus.org/factorial-of-large-numbers/
-
 #include <iostream>
+ #include <string>
+ #include <vector>
+ #include <algorithm>
+ #include <sstream>
+ #include <queue>
+ #include <deque>
+ #include <bitset>
+ #include <iterator>
+ #include <list>
+ #include <stack>
+ #include <map>
+ #include <set>
+ #include <functional>
+ #include <numeric>
+ #include <utility>
+ #include <limits>
+ #include <time.h>
+ #include <math.h>
+ #include <stdio.h>
+ #include <string.h>
+ #include <stdlib.h>
+ #include <assert.h>
+ 
+ using namespace std;
+ 
+ #define ll long long
+ 
+ int main(){
+     int arr[101][200];
+     for(int i = 0; i < 101; i++)
+         for(int j = 0; j < 200; j++)
+             arr[i][j] = 0;
+       
+     arr[0][199] = 1;
+     for(int i = 1; i < 101; i++){
+         for(int j = 199; j >= 1; j--){      
+             arr[i][j] += (arr[i - 1][j] * i);
+             if(arr[i][j] >= 10){
+                 arr[i][j-1]+= (arr[i][j] / 10);
+                 arr[i][j]%=10;
+             } 
+         } 
+     } 
+     int som = 0;
+     for(int i = 0; i < 200; i++)
+         som += arr[100][i];
+     cout << som << "\n";
+     return 0;
+ }
 
-using namespace std;
-const int MAX = 500;
-int multiplyX(int x, int ans[], int size) {
-  int carry = 0;
-
-  for (int i = 0; i < size; i++) {
-    int product = ans[i] * x + carry;
-    ans[i] = product % 10;
-    carry = product / 10;
-  }
-
-  while (carry) {
-    ans[size] = carry % 10;
-    carry /= 10;
-    size++;
-  }
-  return size;
-}
-
-int factorial(int n) {
-  int ans[MAX];
-  ans[0] = 1;
-  int size = 1;
-  for (int i = 2; i <= n; i++)
-    size = multiplyX(i, ans, size);
-  int somDigits = 0;
-  for (int i = size - 1; i >= 0; i--)
-    somDigits += ans[i];
-  return somDigits;
-}
-
-int main() {
-  cout << factorial(100) << "\n";
-  return 0;
-}
