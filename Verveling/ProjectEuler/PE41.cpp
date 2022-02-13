8f8aa2fb333714bab72f85b8bd01503050ffe184
AliEY0 <ali.elyousfi@outlook.be>
2022-02-13 18:53:55 +0100

    {41, 43}

--- Verveling/ProjectEuler/PE41.cpp
+++ Verveling/ProjectEuler/PE41.cpp
@@ -21,6 +21,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <time.h>
 
 using namespace std;
 
@@ -61,10 +62,14 @@ int nPrime(int n){
 }
 
 int main(){
+    clock_t tStart = clock();
+
     int ans = 0;
     for(int i = 1; i <= 9; i++)
         ans = max(ans, nPrime(i));
     cout << ans << "\n";    
+    printf("----------------------------------------------\nTimeime taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+
     return 0;
 }
 
