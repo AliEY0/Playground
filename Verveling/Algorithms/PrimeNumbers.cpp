988cebea01dd0284a60434a1599aeb6a73ae9792
AliEY0 <ali.elyousfi@outlook.be>
2022-03-01 17:21:21 +0100

    {NERDTree testje}

---
+++ Verveling/Algorithms/PrimeNumbers.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <time.h>
+
+using namespace std;
+
+#define ll long long
+//Voor Meer informatie
+//https://cp-algorithms.com/algebra/sieve-of-eratosthenes.html
+
+
+vector<int> sieve(int n){
+    vector<bool> is_prime(n+1, true);
+    vector<int> primes;
+    is_prime[0] = is_prime[1] = false;
+    for (int i = 2; i * i <= n; i++) {
+        if (is_prime[i]) {
+            for (int j = i * i; j <= n; j += i)
+                is_prime[j] = false;
+        }
+    }
+    for(int i = 0; i < n; i++)
+        if(is_prime[i])
+            primes.push_back(i);
+    return primes;
+}
+
+int main(){
+    //Kleine Test
+    /*int n = 10;
+    vector<int> primeNumbers = sieve(n); //Priemgetallen < 10
+    for(int i = 0 ;i < primeNumbers.size(); i++)
+        cout << primeNumbers[i] << "\n";
+    */
+    
+    return 0;
+}
+
+
+
+
